dbf97538eb0eaa01108f407560dd128487d039c7
Adam Brown <adam@novoda.com>
2013-07-29 23:26:55 +0100

    adding icepick- not working atm

--- app/src/main/java/com/ouchadam/sprsrspodcast/presentation/controller/MyActivity.java
+++ app/src/main/java/com/ouchadam/sprsrspodcast/presentation/controller/MyActivity.java
@@ -1,13 +1,22 @@
 package com.ouchadam.sprsrspodcast.presentation.controller;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.github.frankiesardo.icepick.annotation.Icicle;
+import com.github.frankiesardo.icepick.bundle.Bundles;
+import com.novoda.notils.android.Fragments;
 import com.ouchadam.sprsrspodcast.R;
 import com.ouchadam.sprsrspodcast.debug.DebugActivity;
+import com.ouchadam.sprsrspodcast.presentation.item.LatestFragment;
 
 public class MyActivity extends DrawerActivity {
+
+    @Icicle
+    public String activityTitle;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -17,10 +26,43 @@ public class MyActivity extends DrawerActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.ab_debug)  {
+        if (item.getItemId() == R.id.ab_debug) {
             startActivity(new Intent(this, DebugActivity.class));
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onFangCreate(Bundle savedInstanceState) {
+        Bundles.restoreInstanceState(this, savedInstanceState);
+        Log.e("!!!!", "title oncreate : " + activityTitle);
+        getActionBar().setTitle(activityTitle == null ? "" : activityTitle);
+        invalidateOptionsMenu();
+        if (hasEmptyContent()) {
+            showDefaultFragment();
+        }
+    }
+
+    private boolean hasEmptyContent() {
+        return Fragments.findFragmentById(getSupportFragmentManager(), R.id.content_frame) == null;
+    }
+
+    private void showDefaultFragment() {
+        getActionBar().setTitle("Latest");
+        invalidateOptionsMenu();
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new LatestFragment()).commit();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (getActionBar().getTitle() != null) {
+            activityTitle = getActionBar().getTitle().toString();
+        } else {
+            activityTitle = "is null";
+        }
+        Log.e("!!!!", "title onsave : " + activityTitle);
+        Bundles.saveInstanceState(this, outState);
+        super.onSaveInstanceState(outState);
     }
 
 }
