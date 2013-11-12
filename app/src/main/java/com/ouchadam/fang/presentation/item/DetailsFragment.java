c2163ed011126a878fd3d9793aec36c1b15c0cf0
Adam Brown <adam@novoda.com>
2013-11-12 11:46:40 +0000

    adding open in browser placeholder

--- app/src/main/java/com/ouchadam/fang/presentation/item/DetailsFragment.java
+++ app/src/main/java/com/ouchadam/fang/presentation/item/DetailsFragment.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.novoda.notils.caster.Classes;
 import com.novoda.notils.caster.Views;
@@ -90,6 +91,7 @@ public class DetailsFragment extends Fragment {
                 break;
             default:
                 // todo nothing
+                Toast.makeText(getActivity(), "Todo...", Toast.LENGTH_SHORT).show();
 
         }
         return super.onOptionsItemSelected(item);
